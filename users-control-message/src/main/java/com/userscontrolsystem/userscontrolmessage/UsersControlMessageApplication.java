6ed76fad9390b96c2f4d8e6a9f2ef7318b089ff9
Lucas01PL <lucas01_ce@hotmail.com>
2021-11-21 11:25:03 -0300

    Initial commit project users-control-message.

---
+++ users-control-message/src/main/java/com/userscontrolsystem/userscontrolmessage/UsersControlMessageApplication.java
@@ -0,0 +1,13 @@
+package com.userscontrolsystem.userscontrolmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersControlMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersControlMessageApplication.class, args);
+	}
+
+}
---
+++ users-control-message/src/test/java/com/userscontrolsystem/userscontrolmessage/UsersControlMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userscontrolsystem.userscontrolmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersControlMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
